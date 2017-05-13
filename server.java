0bfb9fc606d8558f393c351ecbf1bb93ed159dae
zhaoqiang <zq441290719@gmail.com>
2017-05-13 15:08:43 +0900

    Update server.java

--- server.java
+++ server.java
@@ -1,28 +1,25 @@
 package com.heyang;
-  
-  import java.io.BufferedReader;
-  import java.io.IOException;
-  import java.io.InputStream;
-  import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
-  import java.net.Socket;
+import java.net.Socket;
   
  /*
   * 基于TCP协议的Socket通信，实现用户登录
   * 服务器端
   */
  public class Server {
-   private Socket socket;
-   private BufferedReader br;
      public static void main(String[] args) {
          try {
              // 1、创建一个服务器Socket，即ServerSocket，指定绑定的端口，并监听此端口
              ServerSocket serverSocket = new ServerSocket(9999);
              // 2、调用()方法开始监听，等待客户端的连接
              System.out.println("***服务器即将启动，等待客户端的连接***");
-             socket = serverSocket.accept();// 就会处于阻塞的状态，等待监听 
+             Socket socket = serverSocket.accept();// 就会处于阻塞的状态，等待监听 
 
              // 3、获取输入流，病读取客户端信息
              InputStream is = socket.getInputStream();// 字节输入流
@@ -32,7 +29,7 @@ import java.net.ServerSocket;
              InputStreamReader isr = new InputStreamReader(is);
 
              // 为输入流添加缓冲
-             br = new BufferedReader(isr);
+             BufferedReader br = new BufferedReader(isr);
 
              String info = null;
              if((info = br.readLine())!=null){
@@ -52,7 +49,15 @@ import java.net.ServerSocket;
                 pw.println("student "+i);
                 pw.flush();
              }
-           
+             
+             
+             String strNum = null;
+             if((strNum = br.readLine())!=null){
+            	 int numOfAdc = Integer.valueOf(strNum);
+       	      for(int i = 0; i < numOfAdc; i++)
+       	         System.out.println(br.readLine());   
+             }
+             //System.out.println(br.readLine()); 
              socket.shutdownInput();//关闭输出流
               
              // 关闭资源
@@ -67,10 +72,4 @@ import java.net.ServerSocket;
              e.printStackTrace();
          }
      }
-   private void readStdList(){
-      String strNum = br.readLine();
-      int numOfAdc = Integer.valueOf(strNum);
-      for(int i = 0; i < numOfAdc; i++)
-         System.out.println(br.readLine);
-   }
  }
