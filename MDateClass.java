1837caed4235ff3aa770d7d591fbd8898ad042b5
MRMRMRMAY <meihaoran@naver.com>
2017-05-14 15:22:51 +0900

    Update MDateClass.java

--- MDateClass.java
+++ MDateClass.java
@@ -4,21 +4,15 @@ import java.util.Calendar;
 import java.util.Date;
 public class MDateClass{
     private String[] weekDays = {"Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"};
-    public String getDayOfWeek(){
+    public String getDate(){
         Calendar cal = Calendar.getInstance();
         Date dt=new Date();
         cal.setTime(dt);
-        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
-        if (w < 0)
-            w = 0;
-        return weekDays[w];
-    }
-    public String getHHMM(){
-        Date d = new Date();  
-        System.out.println(d);  
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;   
+        if (w < 0) w = 0;    
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");  
-        String dateNowStr = sdf.format(d);
+        String dateNowStr = sdf.format(dt);
         System.out.println("格式化后的日期：" + dateNowStr);
-        return dateNowStr;
+        return weekDays[w]+"@"+dateNowStr;
     }
 }
